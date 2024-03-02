d301aa7de69338f0b4cd7cb265c1689954d8059e
Theosdad <Obie_1@mail.ru>
2024-03-02 15:27:52 +0300

    add filter handle

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -16,19 +16,34 @@ function ItemsList(props) {
         setSelectedTag(tag);
     };
 
+    const resetFilter = () => {
+        setSelectedTag(null);
+    };
+
     return (
-        <ul className='item-list'>
-            {items
-                .filter(item => !selectedTag || item.tags.includes(selectedTag))
-                .map(item => (
-                    <Item
-                        key={item.id}
-                        {...item}
-                        addToCart={addToCart}
-                        handleTagSelection={handleTagSelection}
-                    />
-                ))}
-        </ul>
+        <div>
+            {selectedTag ?
+                <div className='filter-container'>
+                    <b>Filtered with <span className='tag'>{selectedTag}</span> tag</b>
+                    <span className='tag' onClick={resetFilter}>Reset filter</span>
+                </div> : null
+            }
+
+            <ul className='item-list'>
+                {
+                    items
+                        .filter(item => !selectedTag || item.tags.includes(selectedTag))
+                        .map(item => (
+                            <Item
+                                key={item.id}
+                                {...item}
+                                addToCart={addToCart}
+                                handleTagSelection={handleTagSelection}
+                            />
+                        ))
+                }
+            </ul>
+        </div>
     );
 }
 
